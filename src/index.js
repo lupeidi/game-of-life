5e43d3d614edac5f5930bf3ae5e7b1e0af70b627
Diana <lupeidi@gmail.com>
2020-01-22 13:24:27 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,193 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {ButtonToolbar, MenuItem, DropdownButton} from 'react-bootstrap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Box extends React.Component {
+
+    selectBox = () => {
+        this.props.selectBox(this.props.row, this.props.col);
+    }
+
+    render(){
+        return(
+            <div
+            className = {this.props.boxClass}
+            id = {this.props.id}
+            onClick = {this.selectBox}
+            />
+        );
+    }
+}
+
+class Grid extends React.Component {
+    render() {
+        const width = this.props.cols * 14;
+        var rowsArr = [];
+
+        var boxClass = '';
+        for (var i = 0; i < this.props.rows; i++ ){
+            for (var j = 0; j < this.props.cols; j++ ) {
+                let boxId = i + '_' + j;
+
+                boxClass = this.props.gridFull[i][j] ? 'box on': 'box off';
+                rowsArr.push(
+                    <Box 
+                     boxClass = {boxClass}
+                     key = {boxId}
+                     boxId = {boxId}
+                     row = {i}
+                     col = {j}
+                     selectBox = {this.props.selectBox}
+                     />
+                );
+            }
+        }
+
+        return (
+            <div className='grid' style = {{width: width}}>
+                {rowsArr}              
+            </div>
+        )
+    }
+}
+
+class Buttons extends React.Component {
+
+	handleSelect = (evt) => {
+		this.props.gridSize(evt);
+	}
+
+    render() {
+        return (
+            <div className = 'center'>
+                <ButtonToolbar>
+                    <button className='btn btn-default' onClick = {this.props.playButton}>Play</button>
+                    <button className='btn btn-default' onClick = {this.props.pauseButton}>Pause</button>
+                    <button className="btn btn-default" onClick={this.props.clear}>
+					  Clear
+					</button>
+					<button className="btn btn-default" onClick={this.props.seed}>
+					  Seed
+					</button>
+                </ButtonToolbar>
+            </div>
+        );
+    }
+}
+
+class Main extends React.Component {
+    constructor(){
+        super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+
+        this.state = {
+            generation: 0,
+            gridFull: Array(this.rows).fill().map( () => Array(this.cols).fill(false))
+        }
+    }
+
+    selectBox = (row, col) => { 
+        let gridCopy = arrayClone(this.state.gridFull);
+        gridCopy[row][col] = !gridCopy[row][col];
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+	seed = () => {
+		let gridCopy = arrayClone(this.state.gridFull);
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				if (Math.floor(Math.random() * 4) === 1) {
+					gridCopy[i][j] = true;
+				}
+			}
+		}
+		this.setState({
+			gridFull: gridCopy
+		});
+	}
+
+	clear = () => {
+		var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+		this.setState({
+			gridFull: grid,
+			generation: 0
+		});
+	}
+
+    playButton = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.play, this.speed);
+    }
+
+    play = () => {
+        let g = this.state.gridFull;
+        let g2 = arrayClone(this.state.gridFull);
+
+        for (let i = 0; i < this.rows; i++ ){
+            for (let j = 0; j < this.cols; j++ ) {
+                let count = 0;
+
+                if(i > 0 && g[i-1][j]) count++;
+                if (i > 0 && j > 0 && g[i-1][j-1]) count++;
+                if (i > 0 && j < this.cols && g[i-1][j+1]) count++;
+                if (j < this.cols - 1 && g[i][j+1]) count++;
+                if(j > 0 && g[i][j-1]) count++;
+                if(i<this.rows - 1 && g[i+1][j]) count++;
+                if(i<this.rows - 1 && j > 0 && g[i+1][j-1]) count++;
+                if(i<this.rows - 1 && this.cols - 1 && g[i+1][j+1]) count++;
+                
+                if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+                if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+        }
+
+        this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1
+        })
+    }
+
+    pauseButton = () => {
+        clearInterval(this.intervalId);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The Game of Life</h1>
+                <Buttons
+                    playButton = {this.playButton}
+                    pauseButton = {this.pauseButton}
+                    clear = {this.clear}
+                    seed = {this.seed}
+                />
+                <Grid  
+                    gridFull = {this.state.gridFull}
+                    cols= {this.cols}
+                    rows = {this.rows}
+                    selectBox = {this.selectBox}
+                />
+                <h2>Generations: {this.state.generation}</h2>
+            </div>
+        )
+    }
+}
+
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
